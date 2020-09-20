39e4f25ddb2a0edc7cf82f849a9ed1b7f0a23ce2
BitXplorer <70508247+BitXplorer@users.noreply.github.com>
2020-09-20 23:57:39 +0200

    Testar

--- src/miniProjekt.java
+++ src/miniProjekt.java
@@ -24,18 +24,19 @@ public class miniProjekt {
         while (!exitMenu) { //In this loop the calculator will continue to pop up with until condition !exitMenu changes to true.
             Scanner mode = new Scanner(System.in);
             System.out.print("\n Calculate one of the following: \n 1. Calculate volume of a Sphere. \n 2. Add all integers. \n\n Please select a option: ");
-            int chosenMode = mode.nextInt();
+            int chosenModeInt = mode.nextInt();
+            //TODO försök att rensa bort doubles och chars eller strings innan det sätts till choseModeInt. Kompletta grundkoden finns på GIT!
 
-            if (mode.hasNextInt() && chosenMode <=2 && chosenMode >=1) {
+            if (chosenModeInt >=1 && chosenModeInt <=2) {
 
-                    if (chosenMode == 1) { //If option 1 is chosen it will automatically add a scanner and default message.
+                    if (chosenModeInt == 1) { //If option 1 is chosen it will automatically add a scanner and default message.
                         Scanner input = new Scanner(System.in);
                         System.out.print("Please add a integer as radius for the sphere: ");
 
                         while (!input.hasNextInt()) { //If it's not an integer that is entered this will notice the user and start a new scanner.
                             System.out.println(" You didn't enter a valid integer!");
                             input = new Scanner(System.in);
-                            System.out.print("\n Please add a integer as radius for the sphere: ");
+                            System.out.print("\n Please add a positive integer as radius for the sphere: ");
                         }
                         if (input.hasNextInt()) { //This will be used if the user has entered a correct Integer.
                             int r = input.nextInt();
@@ -43,14 +44,14 @@ public class miniProjekt {
                             System.out.println("\n The volume of the sphere is: " + result + "\nTry something else: ");
                         }
 
-                    } else if (chosenMode == 2) { //If option 2 is chosen it will automatically add a scanner and default message.
+                    } else if (chosenModeInt == 2) { //If option 2 is chosen it will automatically add a scanner and default message.
                         Scanner input = new Scanner(System.in);
                         System.out.print(" Please add a integer of your choice: ");
 
                         while (!input.hasNextInt()) { //If it's not an integer that is entered this will notice the user and start a new scanner.
                             System.out.println(" You didn't enter a valid integer!");
                             input = new Scanner(System.in);
-                            System.out.print("\n Please add a integer of your choice: ");
+                            System.out.print("\n Please add a positive integer of your choice: ");
                         }
                         if (input.hasNextInt()) { //This will be used if the user has entered a correct Integer.
                             int max = input.nextInt();
@@ -58,12 +59,12 @@ public class miniProjekt {
                             System.out.print("\n All integers within the range 1 to " + max + " is " + result);
 
                         }
-                    } else if (chosenMode == 0) { // If the user adds 0 to quit the calculator we'll change exitMenu value which is the foundation for the while loop this is built on.
+                    } else if (chosenModeInt == 0) { // If the user adds 0 to quit the calculator we'll change exitMenu value which is the foundation for the while loop this is built on.
                         System.out.println("Thank you for using this calculator.");
                         exitMenu = true;
                     }
 
-            }  else if (chosenMode >=3 || chosenMode <=0 ) { // This makes incorrect menu input to start the loop over and notify the user about it.
+            }  else if (mode.hasNextDouble() || chosenMode >=3 || chosenMode <=0){ // This makes incorrect menu input to start the loop over and notify the user about it.
                 System.out.println("Incorrect value selected. Please try again: ");
 
             }
