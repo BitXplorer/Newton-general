c7728af053d178bee04940c32d62a08eafd89de6
BitXplorer <70508247+BitXplorer@users.noreply.github.com>
2020-09-23 15:04:38 +0200

    All done except method for positive integer input?

--- src/miniProject.java
+++ src/miniProject.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class miniProject {
-//TODO Meny valideringen tar inte felaktiga integers? Men tar sånt som inte är Integer. Hur göra?
+
     public static int menuInput() {
         Scanner input = new Scanner(System.in);
         int validated = 0;
@@ -14,10 +14,10 @@ public class miniProject {
 
             } else if (input.hasNextInt()) {
                 validated = input.nextInt();
-                if (validated < 0 || validated > 5) {
+                if (validated <= -1 || validated >= 6) {
                     isValid = false;
-                    System.out.print("You have entered a incorrect value. Please enter a valid option2: ");
-                    input.next();
+                    System.out.print("You have entered a incorrect value. Please enter a valid option: ");
+
                 } else {
                     isValid = true;
 
@@ -56,6 +56,17 @@ public class miniProject {
             return vol;
 
         }
+        public static String fraction( int numerator, int denominator) { // Method where we count the volume of a straight cylinder. ( V = π*r*r*h )
+        int result = numerator / denominator;
+        String resultString = Integer.toString(result);
+        return resultString;
+
+        }
+        public static int calcPoint( int k, int m, int x) { // Method where we count the volume of a straight cylinder. ( V = π*r*r*h )
+        int y = k * x + m;
+        return y;
+
+        }
 
         public static int sum (int max) { //Method of calculating all Integers between 1 and Max value. For this we use a for loop.
             int total = 0;
@@ -66,10 +77,10 @@ public class miniProject {
         }
     public static void main(String[] args) {
         boolean exitMenu = false; //Created this value so that we easily can see when the calculator will be closed.
-            int r,h,m,numerator,max,x,unValidated,chosenMode;
+            int r,h,m,k,numerator,denominator,max,x,chosenMode;
 
             while (!exitMenu){
-                System.out.print("\n Calculate one of the following: \n 1. Calculate volume of a Sphere. \n 2. Calculate the volume of a cylinder. \n 4. Add all integers. \n 0. Exit \n\n Please select one of the options above: ");
+                System.out.print("\n Calculate one of the following: \n 1. Calculate volume of a Sphere. \n 2. Calculate the volume of a cylinder.\n 3. Calulate fraction. \n 4. Add all integers. \n 5. Calculate the functional value (y=kx+m). \n 0. Exit \n\n Please select one of the options above: ");
                 chosenMode = menuInput();
 
                 if (chosenMode == 1) { //If option 1 is chosen it will automatically add a scanner and default message.
@@ -86,13 +97,34 @@ public class miniProject {
                     double result = volume(r,h);
                     System.out.println("The volume of the cylinder is: " + result);
 
+                } else if (chosenMode == 3) { //If option 2 is chosen it will automatically add a scanner and default message.
+                    System.out.print("Please add a numerator as an integer of your choice: ");
+                    numerator = getNewInt();
+                    System.out.print("Please add a denominator as an integer of your choice: ");
+                    denominator = getNewInt();
+                    String result = fraction(numerator,denominator);
+                    System.out.println("The result of " + numerator +"/" + denominator + " is: " + result);
+
 
                 } else if (chosenMode == 4) { //If option 2 is chosen it will automatically add a scanner and default message.
-                    System.out.print("Please add a integer of your choice: ");
+                    System.out.print("Please add an integer of your choice: ");
+                    max = getNewInt();
+                    int result = sum(max);
+                    System.out.print("All integers within the range 1 to " + max + " is " + result);
 
 
+                } else if (chosenMode == 5) { //If option 2 is chosen it will automatically add a scanner and default message.
+                    System.out.print("Please add value k as an integer of your choice: ");
+                    k = getNewInt();
+                    System.out.print("Please add value x as an integer of your choice: ");
+                    x = getNewInt();
+                    System.out.print("Please add value m as an integer of your choice: ");
+                    m = getNewInt();
+                    int result = calcPoint(k,m,x);
+                    System.out.println("The result of " + k +"*" + x + "+" + m + " is: " + result);
 
-        } else if (chosenMode == 0) { // If the user adds 0 to quit the calculator we'll change exitMenu value which is the foundation for the while loop this is built on.
+
+                } else if (chosenMode == 0) { // If the user adds 0 to quit the calculator we'll change exitMenu value which is the foundation for the while loop this is built on.
             System.out.println("Thank you for using this calculator.");
             exitMenu = true;
         }
