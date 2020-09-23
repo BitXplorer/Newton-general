b3e165d2187348224c7d0f4f3d1495fea9964df1
BitXplorer <70508247+BitXplorer@users.noreply.github.com>
2020-09-23 15:51:37 +0200

    ** Updated fraction method to be correct. All done except method for positive integer input and comments in code!

--- src/miniProject.java
+++ src/miniProject.java
@@ -11,16 +11,13 @@ public class miniProject {
                 input.next();
                 System.out.print("You have entered a incorrect value. Please enter a valid option: ");
                 isValid = false;
-
             } else if (input.hasNextInt()) {
                 validated = input.nextInt();
                 if (validated < 0 || validated > 5) {
                     isValid = false;
                     System.out.print("You have entered a incorrect value. Please enter a valid option: ");
-
                 } else {
                     isValid = true;
-
                 }
             }
         } while (!isValid); {
@@ -40,9 +37,9 @@ public class miniProject {
             } else {
                 isValid = true;
                 validated = input.nextInt();
-            }
-        } while (!isValid); {
-        }
+                }
+            } while (!isValid); {
+                }
         return validated;
     }
 
@@ -54,18 +51,18 @@ public class miniProject {
         public static double volume( int r, int h) { // Method where we count the volume of a straight cylinder. ( V = π*r*r*h )
             double vol = (3.14*r*r*h);
             return vol;
-
         }
+
         public static String fraction( int numerator, int denominator) { // Method where we count the volume of a straight cylinder. ( V = π*r*r*h )
-        int result = numerator / denominator;
-        String resultString = Integer.toString(result);
-        return resultString;
-
+            if(numerator>=0) return numerator/denominator +" "+ numerator%denominator + "/"+denominator;
+            String s = numerator/denominator-1 + " ";
+            while((int)denominator/(int) denominator<0) numerator+=denominator;
+            return s + numerator%denominator + "/" + denominator;
         }
+
         public static int calcPoint( int k, int m, int x) { // Method where we count the volume of a straight cylinder. ( V = π*r*r*h )
         int y = k * x + m;
         return y;
-
         }
 
         public static int sum (int max) { //Method of calculating all Integers between 1 and Max value. For this we use a for loop.
@@ -75,6 +72,7 @@ public class miniProject {
             }
             return total;
         }
+
     public static void main(String[] args) {
         boolean exitMenu = false; //Created this value so that we easily can see when the calculator will be closed.
             int r,h,m,k,numerator,denominator,max,x,chosenMode;
@@ -103,7 +101,7 @@ public class miniProject {
                     System.out.print("Please add a denominator as an integer of your choice: ");
                     denominator = getNewInt();
                     String result = fraction(numerator,denominator);
-                    System.out.println("The result of " + numerator +"/" + denominator + " is: " + result);
+                    System.out.println("The fractional result of " + numerator +"/" + denominator + " is: " + result);
 
 
                 } else if (chosenMode == 4) { //If option 2 is chosen it will automatically add a scanner and default message.
@@ -125,9 +123,9 @@ public class miniProject {
 
 
                 } else if (chosenMode == 0) { // If the user adds 0 to quit the calculator we'll change exitMenu value which is the foundation for the while loop this is built on.
-            System.out.println("Thank you for using this calculator.");
-            exitMenu = true;
-        }
+                System.out.println("Thank you for using this calculator.");
+                exitMenu = true;
+                }
             }
 
     }
