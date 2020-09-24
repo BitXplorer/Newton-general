0b750422540d5146d47c456359c2f93714d0fdac
BitXplorer <70508247+BitXplorer@users.noreply.github.com>
2020-09-24 09:51:18 +0200

    Final result?

--- src/miniProject.java
+++ src/miniProject.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class miniProject {
 
-    public static int menuInput() {
+    /**
+     * Kalkylator projekt skapat av Grupp 3. Gruppen innehåller Dan, Mikael, Kaysar, Antene.
+     */
+
+    public static int menuInput() { //Method for validating menu input to be relevant to options in menu.
         Scanner input = new Scanner(System.in);
         int validated = 0;
         boolean isValid = false;
@@ -25,7 +29,7 @@ public class miniProject {
         return validated;
     }
 
-    public static int getNewInt() {
+    public static int getNewInt() { //Method for validating functions input to be an integer.
         Scanner input = new Scanner(System.in);
         int validated = 0;
         boolean isValid;
@@ -42,7 +46,8 @@ public class miniProject {
                 }
         return validated;
     }
-    public static int getNewPositiveInt() {
+
+    public static int getNewPositiveInt() { //Method for validating functions input to be a positive integer.
         Scanner input = new Scanner(System.in);
         int validated = 0;
         boolean isValid = false;
@@ -75,41 +80,42 @@ public class miniProject {
             return vol;
         }
 
-        public static String fraction( int numerator, int denominator) { // Method where we count the volume of a straight cylinder. ( V = π*r*r*h )
+        public static String fraction( int numerator, int denominator) { // Method where we show added fractions in mixed form.
             if(numerator>=0) return numerator/denominator +" "+ numerator%denominator + "/"+denominator;
             String s = numerator/denominator-1 + " ";
             while(denominator/denominator<0) numerator+=denominator;
             return s + numerator%denominator + "/" + denominator;
         }
 
-        public static int calcPoint( int k, int m, int x) { // Method where we count the volume of a straight cylinder. ( V = π*r*r*h )
-        int y = k * x + m;
-        return y;
-        }
-
         public static int sum (int max) { //Method of calculating all Integers between 1 and Max value. For this we use a for loop.
             int total = 0;
             for (int i = 1; i <= max; i++){
-                total += i;
+            total += i;
             }
             return total;
         }
 
+        public static int calcPoint( int k, int m, int x) { // Method where we count the functional value. ( y = kx + m )
+        int y = k * x + m;
+        return y;
+        }
+
+
     public static void main(String[] args) {
-        boolean exitMenu = false; //Created this value so that we easily can see when the calculator will be closed.
+        boolean exitMenu = false; //Using this value so that we easily can see when the calculator will close.
             int r,h,m,k,numerator,denominator,max,x,chosenMode;
 
             while (!exitMenu){
                 System.out.print("\n Calculate one of the following: \n 1. Calculate volume of a Sphere. \n 2. Calculate the volume of a cylinder.\n 3. Calculate fraction. \n 4. Add all integers. \n 5. Calculate the functional value (y=kx+m). \n 0. Exit \n\n Please select one of the options above: ");
                 chosenMode = menuInput();
 
-                if (chosenMode == 1) { //If option 1 is chosen it will automatically add a scanner and default message.
+                if (chosenMode == 1) {
                     System.out.print("Please add a radius as an integer of your choice: ");
                     r = getNewInt();
                     double result = volume(r);
                     System.out.println("The volume of the sphere is: " + result + "\nTry something else: ");
 
-                } else if (chosenMode == 2) { //If option 2 is chosen it will automatically add a scanner and default message.
+                } else if (chosenMode == 2) {
                     System.out.print("Please add a radius as an integer of your choice: ");
                     r = getNewInt();
                     System.out.print("Please add a height as an integer of your choice: ");
@@ -117,7 +123,7 @@ public class miniProject {
                     double result = volume(r,h);
                     System.out.println("The volume of the cylinder is: " + result);
 
-                } else if (chosenMode == 3) { //If option 2 is chosen it will automatically add a scanner and default message.
+                } else if (chosenMode == 3) {
                     System.out.print("Please add a positive numerator as an integer of your choice: ");
                     numerator = getNewPositiveInt();
                     System.out.print("Please add a positive denominator as an integer of your choice: ");
@@ -126,14 +132,14 @@ public class miniProject {
                     System.out.println("The fractional result of " + numerator +"/" + denominator + " is: " + result);
 
 
-                } else if (chosenMode == 4) { //If option 2 is chosen it will automatically add a scanner and default message.
+                } else if (chosenMode == 4) {
                     System.out.print("Please add an integer of your choice: ");
-                    max = getNewInt();
+                    max = getNewPositiveInt();
                     int result = sum(max);
                     System.out.print("All integers within the range 1 to " + max + " is " + result);
 
 
-                } else if (chosenMode == 5) { //If option 2 is chosen it will automatically add a scanner and default message.
+                } else if (chosenMode == 5) {
                     System.out.print("Please add value k as an integer of your choice: ");
                     k = getNewInt();
                     System.out.print("Please add value x as an integer of your choice: ");
@@ -144,7 +150,7 @@ public class miniProject {
                     System.out.println("The result of " + k +"*" + x + "+" + m + " is: " + result);
 
 
-                } else if (chosenMode == 0) { // If the user adds 0 to quit the calculator we'll change exitMenu value which is the foundation for the while loop this is built on.
+                } else if (chosenMode == 0) {
                 System.out.println("Thank you for using this calculator.");
                 exitMenu = true;
                 }
